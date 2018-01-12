e5ccf7b95e9238e181df947c1e51b0a063e215cc
Tymoteusz Machowski <tmachows@gmail.com>
2018-01-12 15:55:30 +0100

    add wcs to root path

--- src/main/java/pl/edu/agh/iosr/websitecacheservice/controller/UrlController.java
+++ src/main/java/pl/edu/agh/iosr/websitecacheservice/controller/UrlController.java
@@ -5,10 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.agh.iosr.websitecacheservice.integration.Storage;
 
-@RestController("/wcs")
+@RestController
+@RequestMapping("/wcs")
 @RequiredArgsConstructor
 public class UrlController {
 
